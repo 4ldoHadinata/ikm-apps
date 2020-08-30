4be32b213b6701e28f383f8a530e0fea1dacbe4e
4ldoHadinata <4ldohadinata@gmail.com>
2020-08-30 10:38:54 +0700

    Update Input 4

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,6 +6,9 @@
             background: #00C974;
             color: #FFFFFF;
         }
+        body {
+            background: #00C974;
+        }
         table {
             text-align: center;
         }
@@ -16,61 +19,64 @@
 @endpush
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid pt-4 pb-4">
 
     <!-- Content Row -->
     @foreach ($jenis_pelayanan as $pelayanan)
-    <div class="row">
-        <h1>{{ $pelayanan->nama_pelayanan }}</h1>
-        <div class="table-responsive">
-            <table class="table" width="100%" cellspacing="0">
-                <thead class="thead-green">
-                    <tr>
-                        <th>No</th>
-                        <th class="align-left">Pernyataan</th>
-                        <th>Sangat Baik</th>
-                        <th>Baik</th>
-                        <th>Kurang</th>
-                        <th>Sangat Kurang</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($data as $kuesioner)
-                        <tr>
-                            <td>{{ $no++ }}</td>
-                            <td class="align-left">{{ $kuesioner->soal }}</td>
-                            <form action="{{ route('input') }}" method="POST" id="input{{ $no }}{{ $pelayanan->id }}">
-                                @csrf
-                                <input type="hidden" name="id_pelayanan" value="{{ $pelayanan->id }}">
-                                <input type="hidden" name="id_responden" value="{{ $id }}">
-                                <input type="hidden" name="id_soal" value="{{ $kuesioner->id }}">
-                                <td>
-                                    <input type="radio" name="nilai" value="4">
-                                </td>
-                                <td>
-                                    <input type="radio" name="nilai" value="3">
-                                </td>
-                                <td>
-                                    <input type="radio" name="nilai" value="2">
-                                </td>
-                                <td>
-                                    <input type="radio" name="nilai" value="1">
-                                </td>
-                            </form>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="4" class="text-center">
-                                Data Kosong
-                            </td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
+        <div class="card mb-4">
+            <div class="card-body">
+                    <h2>{{ $pelayanan->nama_pelayanan }}</h2>
+                    <div class="table-responsive">
+                        <table class="table" width="100%" cellspacing="0">
+                            <thead class="thead-green">
+                                <tr>
+                                    <th>No</th>
+                                    <th class="align-left">Pernyataan</th>
+                                    <th>Sangat Baik</th>
+                                    <th>Baik</th>
+                                    <th>Kurang</th>
+                                    <th>Sangat Kurang</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $no = 1 ?>
+                                @forelse ($data as $kuesioner)
+                                    <tr>
+                                        <td>{{ $no++ }}</td>
+                                        <td class="align-left">{{ $kuesioner->soal }}</td>
+                                        <form action="{{ route('input') }}" method="POST" id="input{{ $no }}{{ $pelayanan->id }}">
+                                            @csrf
+                                            <input type="hidden" name="id_pelayanan" value="{{ $pelayanan->id }}">
+                                            <input type="hidden" name="id_responden" value="{{ $id }}">
+                                            <input type="hidden" name="id_soal" value="{{ $kuesioner->id }}">
+                                            <td>
+                                                <input type="radio" name="nilai" value="4">
+                                            </td>
+                                            <td>
+                                                <input type="radio" name="nilai" value="3">
+                                            </td>
+                                            <td>
+                                                <input type="radio" name="nilai" value="2">
+                                            </td>
+                                            <td>
+                                                <input type="radio" name="nilai" value="1">
+                                            </td>
+                                        </form>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="4" class="text-center">
+                                            Data Kosong
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                </div>
+            </div>
         </div>
-    </div>
     @endforeach
-    <button type="submit" id="btnSubmit" class="btn btn-primary mx-3" onclick="submitAll();">Submit</button>
+    <button type="submit" id="btnSubmit" class="btn btn-primary btn-block" onclick="submitAll();">Submit</button>
 
 </div>
 @endsection
